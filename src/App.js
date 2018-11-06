f6350f4e899932afdfccb9e143531a8c52673bf8
alfieqashwa <alfieqahwa@gmail.com>
2018-11-07 00:02:12 +0700

    finished Chapeter2: Component Declaration

--- src/App.js
+++ src/App.js
@@ -55,34 +55,28 @@ const Search = ({ value, onChange, children }) => (
   </form>
 );
 
-const Table = props => {
-  const { list, pattern, onDismiss } = props;
-  return (
-    <div>
-      {list.filter(isSearched(pattern)).map(item => (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <Button onClick={() => onDismiss(item.objectID)} type="button">
-              Dismiss
-            </Button>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+const Table = ({ list, pattern, onDismiss }) => (
+  <div>
+    {list.filter(isSearched(pattern)).map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)} type="button">
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    ))}
+  </div>
+);
 
-const Button = props => {
-  const { onClick, className = '', children } = props;
-  return (
-    <button onClick={onClick} className={className} type="button">
-      {children}
-    </button>
-  );
-};
+const Button = ({ onClick, className = '', children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
