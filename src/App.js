2626bf35edd3f9a5bfe40d3bc2cff2ced82fec36
alfieqashwa <alfieqahwa@gmail.com>
2018-11-07 10:39:45 +0700

    split up component

--- src/App.js
+++ src/App.js
@@ -18,8 +18,19 @@ const data = [
     objectID: 1
   }
 ];
+function isSearched(searchTerm) {
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+}
+
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
   const [list, setList] = useState(data);
+
+  function onSearchChange(e) {
+    setSearchTerm(e.target.value);
+  }
 
   function onDismiss(id) {
     const isNotId = item => item.objectID !== id;
@@ -29,21 +40,43 @@ export default function App() {
 
   return (
     <div className="App">
-      {list.map(item => (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <button onClick={() => onDismiss(item.objectID)} type="button">
-              > Dismiss
-            </button>
-          </span>
-        </div>
-      ))}
+      <Search value={searchTerm} onChange={onSearchChange}>
+        Search
+      </Search>
+      <Table list={list} pattern={searchTerm} onDismiss={onDismiss} />
     </div>
   );
 }
+
+const Search = ({ value, onChange, children }) => (
+  <form>
+    {children}
+    <input type="text" value={value} onChange={onChange} />
+  </form>
+);
+
+const Table = ({ list, pattern, onDismiss }) => (
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item => (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)} type="button">
+            > Dismiss
+          </Button>
+        </span>
+      </div>
+    ))}
+  </div>
+);
+
+const Button = ({ onClick, className = '', children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
